4dd6443c110ee32f1e28f3771be2d5a5bab9cd9a
Tobias Ephron <tobiasephron@gmail.com>
2022-04-23 16:51:08 -0400

    Start menu complete; added new scenes for city and suburbs

---
+++ Vassar_hacks_2022_game/Assets/Scripts/menuControl.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class menuControl : MonoBehaviour
+{
+    [Header("Levels To Load")]
+    public string _cityLevel;
+    public string _suburbsLevel;
+
+    public void CityDialogYes(){
+        SceneManager.LoadScene(_cityLevel);
+    }
+
+    public void SuburbsDialogYes(){
+        SceneManager.LoadScene(_suburbsLevel);
+    }
+
+    public void QuitButton(){
+        Application.Quit();
+    }
+}
